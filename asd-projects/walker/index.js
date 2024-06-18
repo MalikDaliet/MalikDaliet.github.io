bb0937abc5dc23d0c184cc8f8fff820e83dee35e
Malik Daliet <malikdaliet1@gmail.com>
2024-06-18 20:58:31 +0000

    tod0 7

--- asd-projects/walker/index.js
+++ asd-projects/walker/index.js
@@ -10,74 +10,102 @@ function runProgram() {
   $(document).on('keydown', handleKeyDown);
 
   // CORE LOGIC...
+
+
+
+
+  // Constant Variables
+  var FRAME_RATE = 60;
+  var FRAMES_PER_SECOND_INTERVAL = 1000 / FRAME_RATE;
+  const KEY = {
+    ENTER: 13,
+    LEFT: 37,
+    RIGHT: 39,
+    UP: 38,
+    DOWN: 40
+  };
+  // Game Item Objects
+
+  var walker = {
+    x: 0,
+    y: 0,
+    speedXAxis: 0,
+    speedYAxis: 0
+  }
+
+
+  // one-time setup
+  var interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
+  $(document).on('eventType', handleEvent);                           // change 'eventType' to the type of event you want to handle
+
+  ////////////////////////////////////////////////////////////////////////////////
+  ///////////////////////// CORE LOGIC ///////////////////////////////////////////
+  ////////////////////////////////////////////////////////////////////////////////
+
+  /* 
+  On each "tick" of the timer, a new frame is dynamically drawn using JavaScript
+  by calling this function and executing the code inside.
+  */
   //  making my buttons work
   function handleKeyDown(event) {
-    if (event.which === KEY.DOWN){
-    console.log("down key");}
-    function handleKeyUp(event) {
-      if (event.which === KEY.UP) {
-        console.log("enter pressed");
-        console.log("up key")
-      }
-      function handleKeyRight(event) {
-        if (event.which === KEY.RIGHT) {
-          console.log("right key")
-        }
-        function handleLeft(event) {
-          if (event.which === KEY.LEFT) {
-            console.log("left key")
-          }
-        }
-      }
-    }
-    // Constant Variables
-    var FRAME_RATE = 60;
-    var FRAMES_PER_SECOND_INTERVAL = 1000 / FRAME_RATE;
-    const KEY = {
-      ENTER: 13,
-      LEFT: 37,
-      RIGHT: 39,
-      UP: 38,
-      DOWN: 40
-    };
-    // Game Item Objects
-
-
-    // one-time setup
-    var interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
-    $(document).on('eventType', handleEvent);                           // change 'eventType' to the type of event you want to handle
-
-    ////////////////////////////////////////////////////////////////////////////////
-    ///////////////////////// CORE LOGIC ///////////////////////////////////////////
-    ////////////////////////////////////////////////////////////////////////////////
-
-    /* 
-    On each "tick" of the timer, a new frame is dynamically drawn using JavaScript
-    by calling this function and executing the code inside.
-    */
-    function newFrame() {
-
-
+    if (event.which === KEY.DOWN) {
+      walker.speedYAxis = +5
+      console.log("down key");
     }
 
-    /* 
-    Called in response to events.
-    */
-    function handleEvent(event) {
-
+    if (event.which === KEY.UP) {
+     walker.speedYAxis = -5
+      console.log("up key")
     }
 
-    ////////////////////////////////////////////////////////////////////////////////
-    ////////////////////////// HELPER FUNCTIONS ////////////////////////////////////
-    ////////////////////////////////////////////////////////////////////////////////
-
-
-    function endGame() {
-      // stop the interval timer
-      clearInterval(interval);
-
-      // turn off event handlers
-      $(document).off();
+    if (event.which === KEY.RIGHT) {
+      walker.speedXAxis = +5
+      console.log("right key")
     }
+
+    if (event.which === KEY.LEFT) {
+     walker.speedXAxis = -5
+      console.log("left key")
+    }
+    if (event.which === KEY.ENTER) {
+      console.log("enter pressed");
+    }
+  }
+  function repositionGameItem() {
+    walker.x += walker.speedXAxis
+    walker.y += walker.speedYAxis
+  }
+
+  function redrawGameItem() {
+    $("#walker").css("left", walker.x)
+    $("#walker").css("top", walker.y);
+  }
+
+  function newFrame() {
+    repositionGameItem()
+    redrawGameItem()
 
   }
+
+  /* 
+  Called in response to events.
+  */
+  function handleEvent(event) {
+
+  }
+
+  ////////////////////////////////////////////////////////////////////////////////
+  ////////////////////////// HELPER FUNCTIONS ////////////////////////////////////
+  ////////////////////////////////////////////////////////////////////////////////
+
+
+  function endGame() {
+    // stop the interval timer
+    clearInterval(interval);
+
+    // turn off event handlers
+    $(document).off();
+  }
+
+}
+
