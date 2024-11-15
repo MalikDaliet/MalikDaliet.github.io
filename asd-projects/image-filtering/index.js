549eff551966b92fb76a18ca82256fcaed48100d
MalikDaliet <135252849+MalikDaliet@users.noreply.github.com>
2024-11-15 01:59:40 +0000

    todo 7 of imagae filtering

--- asd-projects/image-filtering/index.js
+++ asd-projects/image-filtering/index.js
@@ -20,8 +20,8 @@ function resetAndRender() {
 // all of your apply functions
 function applyAndRender() {
   // Multiple TODOs: Call your apply function(s) here
+  applyFiller(reddify)
 
-  
 
   // do not change the below line of code
   render($("#display"), image);
@@ -32,18 +32,49 @@ function applyAndRender() {
 /////////////////////////////////////////////////////////
 
 // TODO 1, 2 & 4: Create the applyFilter function here
-
+function applyFiller(filterFunction) {
+  for (let i = 0; i < image.length; i++) {
+    let row = image[i]
+    for (let j = 0; j < row.length; j++) {
+      let rgbString = row[j]
+      let rgbNumbers = rgbArrayToString(rgbNumbers)
+      filterFunction (rgbNumbers)
+    }
+  }
+}
 
 // TODO 7: Create the applyFilterNoBackground function
+function applyFilterNoBackground() {
+  function applyFiller(filterFunction) {
+  for (let i = 0; i < image.length; i++) {
+    let row = image[i]
+    for (let j = 0; j < row.length; j++) {
+      let rgbString = row[j]
+      let rgbNumbers = rgbArrayToString(rgbNumbers)
+     if (rgb)
+      filterFunction (rgbNumbers)
+    }
+  }
+}
 
+}
 
 // TODO 5: Create the keepInBounds function
-
+function keepInBounds (arr) {
+  arr = Math.max(arr, 0)
+  arr = Math.min (arr, 255)
+}
 
 // TODO 3: Create reddify function
-
+function reddify(arr){
+  arr[RED]= 200 
+}
 
 // TODO 6: Create more filter functions
-
-
+function decreaseBlue (arr){
+  arr[BLUE]= keepInBounds (arr[BLUE - 50]) 
+}
+function increaseGreenByBlue (arr ) {
+  arr [GREEN] = keepInBounds (arr[BLUE] + arr[GREEN])
+}
 // CHALLENGE code goes below here
