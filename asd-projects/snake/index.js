c2059a97f43e0c0d8e0ca7e88338f52d601da94b
MalikDaliet <135252849+MalikDaliet@users.noreply.github.com>
2024-11-26 21:22:16 +0000

    im the procces of completing snake

--- asd-projects/snake/index.js
+++ asd-projects/snake/index.js
@@ -50,10 +50,12 @@ snake.body = [];
 makeSnakeSquare(10, 10);
 snake.head = snake.body[0];
   // TODO 4b-2: initialize the apple
-  makeApple()
+   makeApple()
+
+ }
   // TODO 5a: Initialize the interval
 
-}
+
 
 ////////////////////////////////////////////////////////////////////////////////
 ///////////////////////// PROGRAM FUNCTIONS ////////////////////////////////////
@@ -192,7 +194,19 @@ function endGame() {
  */
 function makeApple() {
   // TODO 4b-1: Fill in the makeApple() code block
-}
+    apple.element = $("<div>").addClass("apple").appendTo(board);
+  
+    // get a random available row/column on the board
+    var randomPosition = getRandomAvailablePosition();
+  
+    // initialize the row/column properties on the Apple Object
+    apple.row = randomPosition.row;
+    apple.column = randomPosition.column;
+  
+    // position the apple on the screen
+    repositionSquare(apple);
+  }
+
 
 /* Create an HTML element for a snakeSquare using jQuery. Then, given a row and
  * column on the board, position it on the screen. Finally, add the new
@@ -200,7 +214,29 @@ function makeApple() {
  */
 function makeSnakeSquare(row, column) {
   // TODO 4c-1: Fill in this function's code block
-}
+   // initialize a new snakeSquare Object
+   var snakeSquare = {};
+
+   // make the snakeSquare.element Object and append it to the board
+   snakeSquare.element = $("<div>").addClass("snake").appendTo(board);
+ 
+   // initialize the row and column properties on the snakeSquare Object
+   snakeSquare.row = row;
+   snakeSquare.column = column;
+ 
+   // set the position of the snake on the screen
+   repositionSquare(snakeSquare);
+ 
+   // if this is the head, add the snake-head id
+   if (snake.body.length === 0) {
+     snakeSquare.element.attr("id", "#snake-head");
+   }
+ 
+   // add snakeSquare to the end of the body Array and set it as the new tail
+   snake.body.push(snakeSquare);
+   snake.tail = snakeSquare;
+ }
+
 
 /* 
   event.which returns the keycode of the key that is pressed when the
